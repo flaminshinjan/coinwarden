5964f680df4f0105c3fcacf6f7e31f487b5adcaa
daksh <dakshyt1205@gmail.com>
2024-01-20 21:49:41 +0530

    Sign In Skeleton Done

---
+++ lib/auth/sign_in.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In'),
+      ),
+      body: const Center(
+        child: Text('Sign In'),
+      ),
+    );
+  }
+}
